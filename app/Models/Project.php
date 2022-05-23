c2894346e2426171adf7b3303e2f421cb4f092c2
Hosseinreza <hrakbari77@gmail.com>
2022-05-23 05:21:01 +0430

    Add collection to project model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,7 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
+/**
+ * @property string _id
+ * @property string status
+ * @property string[] userIDs
+ */
+
 class Project extends Model
 {
     use HasFactory;
+
+
+    protected $collection = "tm_projects";
+
+
+
 }
