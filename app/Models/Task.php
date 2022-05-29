ab39de593fc7bbbed5448a80e43e1c2606f58609
Hosseinreza <hrakbari77@gmail.com>
2022-05-29 05:29:56 +0430

    Add property for Task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -5,6 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
+/**
+ * @property string _id
+ *
+ */
 class Task extends Model
 {
     use HasFactory;
