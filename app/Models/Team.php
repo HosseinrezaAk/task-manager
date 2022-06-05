5bf7578970959500b5fc25d371d7b0f65d85e200
Hosseinreza <hrakbari77@gmail.com>
2022-06-05 10:10:35 +0430

    Add name property to Team

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -5,6 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
+/**
+ * @property string name
+ */
 class Team extends Model
 {
     use HasFactory;
