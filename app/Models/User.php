1ac7c4a4d742bac8bff3611a986fe2715f913e67
Hosseinreza <hrakbari77@gmail.com>
2022-06-13 09:56:47 +0430

    Add teams

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,4 +53,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function teams(){
+        return $this->belongsToMany(Team::class,"","","");
+    }
 }
