8616fb09f5650a4dd7b10fe5c151b5ca8b95c18d
Hosseinreza <hrakbari77@gmail.com>
2022-06-16 10:06:41 +0430

    change id to _id

--- database/migrations/2022_06_14_052919_create_team_user_table.php
+++ database/migrations/2022_06_14_052919_create_team_user_table.php
@@ -18,7 +18,8 @@ return new class extends Migration
         Schema::create(
             $this->collection,
             function (Blueprint $table) {
-                $table->id();
+//                $table->id();
+                $table->string("_id");
                 $table->timestamps();
         });
     }
