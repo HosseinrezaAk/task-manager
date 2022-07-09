2ed050515cd18d0238142b7ca4a031e66398094f
Hosseinreza <hrakbari77@gmail.com>
2022-07-09 21:23:15 +0430

    Add delete route for user

--- routes/user/routes.php
+++ routes/user/routes.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 
         Route::post('/create',[UserController::class,'store']);
+        Route::delete('/delete',[UserController::class,'delete']);
         /**
          * get-users: for showing all the users
          * delete-multiple-users
