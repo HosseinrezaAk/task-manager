88ba7519eea3ef903b593be4ec5dc7e1273fa061
Hosseinreza <hrakbari77@gmail.com>
2022-07-21 11:16:16 +0430

    Add update in teams

--- routes/team/routes.php
+++ routes/team/routes.php
@@ -1,1 +1,31 @@
 <?php
+
+
+use App\Http\Controllers\TeamController;
+use Illuminate\Support\Facades\Route;
+
+
+
+
+Route::group(
+    ['prefix' => 'team'],
+    function() {
+        /**
+         * update
+         */
+
+        Route::get('show-all-teams',[TeamController::class,'showAllTeams']);
+        Route::post('get-teams',[TeamController::class,'getTeams']); // show teams with paginator
+        Route::get('get-team-leader',[TeamController::class,'getTeamLeader']);
+        Route::post('delete-multiple-teams',[TeamController::class,'deleteMultiple']);
+        Route::get('get-members-tasks', [TeamController::class,'getMembersTasks']);
+    }
+);
+Route::group(
+    ['prefix' => 'team','middleware'=>['auth:api', 'isActive']],
+    function() {
+        Route::get('get-members-tasks', [TeamController::class,'getMembersTasks']);
+    }
+);
+Route::resource('/teams', 'TeamController')->middleware(['auth:api', 'isActive']);
+
