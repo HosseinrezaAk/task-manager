a0d8569409ffcfb4deac4352770390a8ffe7e36f
Hosseinreza <hrakbari77@gmail.com>
2022-07-23 11:24:37 +0430

    delete

--- routes/team/routes.php
+++ routes/team/routes.php
@@ -18,17 +18,8 @@ Route::group(
          * get-member-task : team leader will see the tasks of specific member
          */
 
-
-
-
-
     }
 );
-Route::group(
-    ['prefix' => 'team','middleware'=>['auth:api', 'isActive']],
-    function() {
-        Route::get('get-members-tasks', [TeamController::class,'getMembersTasks']);
-    }
-);
+
 Route::resource('/teams', 'TeamController')->middleware(['auth:api', 'isActive']);
 
