a13e92f0e4d9944c03be363706b25e1c93e5f9c6
Hosseinreza <hrakbari77@gmail.com>
2022-08-03 06:45:00 +0430

    Comment email field for users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
 //            $table->id();
             $table->string('id')->unique();
             $table->string('name');
-            $table->string('email')->unique();
+//            $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
