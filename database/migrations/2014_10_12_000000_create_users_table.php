78da3de8c5322c0709c0d737e16dec29331c32a9
Hosseinreza <hrakbari77@gmail.com>
2022-08-04 06:38:11 +0430

    Add string id to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,8 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+//            $table->id();
+            $table->string('id')->unique();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
