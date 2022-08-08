d865cd249c0f81b4beba9f3cbd506ae70b01ba68
Hosseinreza <hrakbari77@gmail.com>
2022-08-08 20:04:10 +0430

    Add route for creating user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -37,7 +37,7 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
     }
 
     /**
--- routes/user/routes.php
+++ routes/user/routes.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 Route::group(
     ['prefix' => 'users'],
     function () {
-
+        Route::post('create',[UserController::class,'store']);
         /**
          * get-users: for showing all the users
          * delete-multiple-users
@@ -20,6 +20,7 @@ Route::group(
          * show: to show specific user
          * update : to update single row
          */
+
 
     }
 );
