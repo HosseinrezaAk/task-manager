fcdf4c24dd211652001098734bb638e55b73d542
Hosseinreza <hrakbari77@gmail.com>
2022-08-09 15:59:39 +0430

    Add team collection

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
 
+
+/**
+ * @property string _id
+ * @property string name
+ */
 class Team extends Model
 {
     use HasFactory;
+
+    protected $collection = "tm_team";
+
+
 }
