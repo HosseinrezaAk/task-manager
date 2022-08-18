33549bb2f133c55ac7745e9039ad4d6c3f293703
Hosseinreza <hrakbari77@gmail.com>
2022-08-18 18:53:09 +0430

    Add store method for team

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Team;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class TeamController extends Controller
@@ -16,25 +18,29 @@ class TeamController extends Controller
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return array
      */
     public function store(Request $request)
     {
-        //
+        $teamData = $request->all();
+        $team  = new Team;
+        $team->name = $teamData['name'];
+        $userIDs = $teamData['userIDs'];
+        foreach($userIDs as $userID)
+        {
+            $user = User::find($userID);
+            $user->teams()->save(new Team(['name'=>$teamData['name']]));
+        }
+        return [
+          'status'=> 'success',
+
+        ];
     }
 
     /**
