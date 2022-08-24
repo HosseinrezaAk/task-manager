9f11610eff56ea98c9bfa3b8671a928da7fdbae1
Hosseinreza <hrakbari77@gmail.com>
2022-08-24 17:48:40 +0430

    Add jenssegers to Project

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -3,8 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
+//use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
