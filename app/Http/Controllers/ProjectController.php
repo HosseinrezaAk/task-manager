894957b29a87557c0ac95738a9484f4a4629ee4a
Hosseinreza <hrakbari77@gmail.com>
2022-08-30 20:59:24 +0430

    Add store method in project

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -34,7 +34,7 @@ class ProjectController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
     }
 
     /**
--- routes/project/routes.php
+++ routes/project/routes.php
@@ -1,10 +1,11 @@
 <?php
 
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 // define admin routes here
 
-
+    Route::post('/create',[ProjectController::class, 'store']);
         /**
          * get-all-projects
          * get-projects
