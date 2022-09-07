919f09d6af893ca07327b8a9a0a5dafb41dad1b7
Hosseinreza <hrakbari77@gmail.com>
2022-09-07 23:51:42 +0430

    Delete useless func

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,15 +23,6 @@ class UserController extends Controller
             ];
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -61,16 +52,7 @@ class UserController extends Controller
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+
 
     /**
      * Update the specified resource in storage.
