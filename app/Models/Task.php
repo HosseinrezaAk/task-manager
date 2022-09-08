463c267b5735dbf521ece491e63d930a4bc03b4a
Hosseinreza <hrakbari77@gmail.com>
2022-09-09 01:32:02 +0430

    add assignee relation

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -15,7 +15,13 @@ class Task extends Model
         'creator'
     ];
 
+
     public function project(){
         return $this->belongsTo(Project::class);
     }
+
+    public function assignee(){
+
+    }
+
 }
