7b7fa657ab561ca5f8e51bb218e181c756549dc3
Hosseinreza <hrakbari77@gmail.com>
2022-09-11 01:37:22 +0430

    Add creatorID

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -25,7 +25,7 @@ class Task extends Model
     }
 
     public function creator(){
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'creatorID');
     }
 
 }
