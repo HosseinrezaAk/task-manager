679dd491813d13a1e30a5af66856339c35f69669
Hosseinreza <hrakbari77@gmail.com>
2022-09-11 01:37:34 +0430

    Add projectID

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -17,7 +17,7 @@ class Task extends Model
 
 
     public function project(){
-        return $this->belongsTo(Project::class);
+        return $this->belongsTo(Project::class,'projectID');
     }
 
     public function assignee(){
