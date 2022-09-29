6ff012d32c9d23ff418cac7214625c5f84b2a55d
Hosseinreza <hrakbari77@gmail.com>
2022-09-29 11:44:47 +0330

    import

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 //use Illuminate\Database\Eloquent\Model;
 use Jenssegers\Mongodb\Eloquent\Model;
+use Jenssegers\Mongodb\Relations\BelongsTo;
 use Jenssegers\Mongodb\Relations\BelongsToMany;
 
 /**
