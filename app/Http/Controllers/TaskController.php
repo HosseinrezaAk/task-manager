ca318537977a75b3108f3a60a2db8fb6a519e915
Hosseinreza <hrakbari77@gmail.com>
2022-11-05 15:42:30 +0330

    Add taskID

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -106,7 +106,7 @@ class TaskController extends Controller
      * @param  string  $taskID
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy($taskID)
     {
         //
     }
