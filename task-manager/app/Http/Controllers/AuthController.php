287391e67426c64311cf6d6a53acb8f5f4d65c8c
Hosseinreza <hrakbari77@gmail.com>
2023-01-11 15:36:22 +0330

    Add register method

--- task-manager/app/Http/Controllers/AuthController.php
+++ task-manager/app/Http/Controllers/AuthController.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function register(Request $request){
 
+    public function register(Request $request){
+        $userData = $request->all();
+        User::create([
+            "name" => $userData["name"],
+            "password" => Hash::make( $userData["password"] )
+        ]);
     }
 }
