3fa86843671eebe2ff91212930d3647c14fae264
Hosseinreza <hrakbari77@gmail.com>
2023-01-14 15:52:21 +0330

    Add login controller

--- task-manager/app/Http/Controllers/AuthController.php
+++ task-manager/app/Http/Controllers/AuthController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
@@ -15,5 +17,14 @@ class AuthController extends Controller
             "name" => $userData["name"],
             "password" => Hash::make( $userData["password"] )
         ]);
+    }
+
+    public function login(Request $request){
+        if( !Auth::attempt($request->only(["name","password"])) )
+        {
+            return response([
+                "message" => "Invalid Credential"
+            ], Response::HTTP_UNAUTHORIZED);
+        }
     }
 }
