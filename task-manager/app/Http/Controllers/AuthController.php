b83c26a19a6c1cea1bc1144b675fb3f72c6449e7
Hosseinreza <hrakbari77@gmail.com>
2023-03-17 21:13:44 +0330

    Test register Route

--- task-manager/app/Http/Controllers/AuthController.php
+++ task-manager/app/Http/Controllers/AuthController.php
@@ -3,12 +3,81 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Response;
-
+use Illuminate\Support\Facades\Validator;
 class AuthController extends Controller
 {
 
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return JsonResponse
+     */
+
+
+
+    public function register(Request $request)
+    {
+        $credentials = $request->only('username', 'password');
+        $validator = Validator::make($request->all(), [
+            'username' => 'required|string|max:255',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $user = User::create([
+            'username' => $request->username,
+            'password' => bcrypt($request->password),
+        ]);
+
+        $token = $this->guard()->attempt($credentials);
+
+        return response()->json([
+            'token' => $token,
+            'user' => $user
+        ]);
+    }
+
+
+
+
+
+
+    /**
+     * Get a JWT token via given credentials.
+     *
+     * @param Request $request
+     *
+     * @return JsonResponse
+     */
+    public function login(Request $request)
+    {
+        $credentials = $request->only('username', 'password');
+
+        if ($token = $this->guard()->attempt($credentials)) {
+            return $this->respondWithToken($token);
+        }
+
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+
+
+
+    /**
+     * Get the guard to be used during authentication.
+     *
+     * @return Guard
+     */
+    public function guard()
+    {
+        return Auth::guard();
+    }
 }
--- task-manager/routes/auth/routes.php
+++ task-manager/routes/auth/routes.php
@@ -4,6 +4,19 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 
-//    Route::post('register', [AuthController::class,'register']);
+    Route::post('register', [AuthController::class,'register']);
 //    Route::post('login', [AuthController::class,'login']);
 
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('register', [AuthController::class,'register']);
+    Route::post('login', [AuthController::class,"login"]);
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
