78579bd03beb0749ddc921215356861b8821cb9f
Hosseinreza <hrakbari77@gmail.com>
2023-03-17 23:14:31 +0330

    Add authenticate middleware

--- task-manager/app/Http/Middleware/Authenticate.php
+++ task-manager/app/Http/Middleware/Authenticate.php
@@ -3,19 +3,24 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
-
-class Authenticate extends Middleware
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Closure;
+class Authenticate
 {
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string|null
-     */
-    protected function redirectTo($request)
+    public function handle($request, Closure $next)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+        } catch (\Exception $e) {
+            if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
+                return response()->json(['error' => 'Invalid token'], 401);
+            } else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
+                return response()->json(['error' => 'Token expired'], 401);
+            } else {
+                return response()->json(['error' => 'Token not found'], 401);
+            }
         }
+
+        return $next($request);
     }
 }
