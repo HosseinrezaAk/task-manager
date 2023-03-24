ee0ffb63c176bf812d6e7a0870941a19226a28a3
Hosseinreza <hrakbari77@gmail.com>
2023-03-24 17:32:48 +0430

    Add more detail to tests

--- TaskManagerApp/tests/Feature/UserAuthTest.php
+++ TaskManagerApp/tests/Feature/UserAuthTest.php
@@ -2,11 +2,12 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class UserLoginTest extends TestCase
+class UserAuthTest extends TestCase
 {
     /**
      * A basic feature test example.
@@ -16,7 +17,7 @@ class UserLoginTest extends TestCase
     public function test_register_user()
     {
         $response = $this->postJson('/auth/register',[
-            "username"=> "newUser3",
+            "username"=> "newUser6",
             "password"=> "123H@1234"
         ]);
 
@@ -29,9 +30,14 @@ class UserLoginTest extends TestCase
 
 
     public function test_login_user(){
+
+        $user = User::factory()->create([
+            'username' => 'newUser5',
+            'password' => bcrypt('password'),
+        ]);
         $response = $this->postJson("/auth/login",[
-            "username"=> "newUser2",
-            "password"=> "123H@1234"
+            "username"=> "newUser5",
+            "password"=> "password"
         ]);
 
         $response
@@ -39,5 +45,6 @@ class UserLoginTest extends TestCase
             ->assertJson([
                 "access_token"=> true
             ]);
+        $this->assertAuthenticatedAs($user);
     }
 }
