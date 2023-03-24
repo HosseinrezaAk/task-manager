9be0e129a21b52470e7e68faab94a9ee2b759190
Hosseinreza <hrakbari77@gmail.com>
2023-03-24 18:18:08 +0430

    Add logout test

--- TaskManagerApp/tests/Feature/UserAuthTest.php
+++ TaskManagerApp/tests/Feature/UserAuthTest.php
@@ -3,9 +3,11 @@
 namespace Tests\Feature;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class UserAuthTest extends TestCase
 {
@@ -14,37 +16,63 @@ class UserAuthTest extends TestCase
      *
      * @return void
      */
-    public function test_register_user()
+//    public function test_register_user()
+//    {
+//        $response = $this->postJson('/auth/register',[
+//            "username"=> "newUser6",
+//            "password"=> "123H@1234"
+//        ]);
+//
+//        $response
+//            ->assertStatus(200)
+//            ->assertJson([
+//                "token" => true
+//            ]);
+//    }
+
+
+//    public function test_login_user(){
+//
+//        $user = User::factory()->create([
+//            'username' => 'newUser5',
+//            'password' => bcrypt('password'),
+//        ]);
+//        $response = $this->postJson("/auth/login",[
+//            "username"=> "newUser5",
+//            "password"=> "password"
+//        ]);
+//
+//        $response
+//            ->assertStatus(200)
+//            ->assertJson([
+//                "access_token"=> true
+//            ]);
+//    }
+
+    public function test_logout_user()
     {
-        $response = $this->postJson('/auth/register',[
-            "username"=> "newUser6",
-            "password"=> "123H@1234"
-        ]);
-
-        $response
-            ->assertStatus(200)
-            ->assertJson([
-                "token" => true
-            ]);
-    }
-
-
-    public function test_login_user(){
-
-        $user = User::factory()->create([
-            'username' => 'newUser5',
+        // Create a user
+        $user = User::create([
+            'username' => 'newUser2',
             'password' => bcrypt('password'),
         ]);
-        $response = $this->postJson("/auth/login",[
-            "username"=> "newUser5",
-            "password"=> "password"
-        ]);
+        $credentials = [
+            "username" => "newUser2",
+            "password" => "password"
+        ];
+        $token = JWTAuth::attempt($credentials);
 
-        $response
-            ->assertStatus(200)
-            ->assertJson([
-                "access_token"=> true
-            ]);
-        $this->assertAuthenticatedAs($user);
+        // Login the user
+        $response = $this
+            ->actingAs($user)
+            ->withHeaders([
+                "Authorization" => "Bearer" . $token,
+            ])
+            ->post("/auth/logout");
+
+        $this->assertFalse(Auth::check());
+        $response->assertStatus(200);
     }
+
+
 }
